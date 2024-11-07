2c0ad95dd8f2a50dce882e5de092d6d63021cecf
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-11-07 06:58:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import StateFulForm from "./components/StateFulForm/StateFulForm";
+// import SimpleForm from "./components/SimpleForm/SimpleForm";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      {/* <SimpleForm /> */}
+      <StateFulForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/_form_.js
@@ -0,0 +1,5 @@
+/**
+ * 1. use form tag and onSubmit handler with event (e) from e access e.target.[name of the input field].value
+ *
+ * 2. use indivdual field state
+ */
